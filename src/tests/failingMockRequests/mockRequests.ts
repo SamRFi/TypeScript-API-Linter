70399d878256ca2573bd38312ae80fae69d94e9d
sam.rafie <sam.rafie@student.howest.be>
2024-03-01 17:21:53 +0100

    assertion of failed endpoint matching also works, and displays correct error message regarding discrepancy

---
+++ src/tests/failingMockRequests/mockRequests.ts
@@ -0,0 +1,24 @@
+//mock test assertion so suite doesn't fail
+test('mock test', () => {
+  expect(1).toBe(1);
+});
+
+// tests/mockRequests.ts
+async function fetchData() {
+    const response = await fetch('https://example.com/auth/signin', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user: 'test', password: 'test' }),
+    });
+  
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+  
+    return response.json();
+  }
+  
+  export { fetchData };
+  
--- src/tests/linterTests.ts
+++ src/tests/linterTests.ts
@@ -7,7 +7,7 @@ import path from 'path';
 describe('Linter Functionality', () => {
   it('should detect mismatched endpoints between TS files and Postman collection', () => {
     // Adjust the path to the directory containing your mock TypeScript file
-    const tsFilesPath = path.join(__dirname, 'mockRequests'); // Assuming mockRequests.ts is in the tests directory
+    const tsFilesPath = path.join(__dirname, 'succeedingMockRequests'); // Assuming mockRequests.ts is in the tests directory
 
     // Use tsParser to parse TypeScript files in the directory and get endpoints
     const tsEndpoints: TSEndpoint[] = tsParser(tsFilesPath);
@@ -24,3 +24,29 @@ describe('Linter Functionality', () => {
     expect(errors).toEqual([]);
   });
 });
+
+describe('Linter Functionality - Failure Cases', () => {
+  it('should correctly identify mismatched endpoints between TS files and Postman collection', () => {
+    // Path to the directory containing the intentionally failing mock TypeScript files
+    const tsFilesPath = path.join(__dirname, 'failingMockRequests');
+
+    // Use tsParser to parse TypeScript files in the directory and get endpoints
+    const tsEndpoints: TSEndpoint[] = tsParser(tsFilesPath);
+
+    // Path to the Postman collection JSON file
+    const collectionFilePath = path.join(__dirname, 'mockPostmanCollection.json');
+    const postmanCollection = readPostmanCollection(collectionFilePath);
+    const postmanEndpoints = parseCollection(postmanCollection);
+
+    // Run the linting rule checker
+    const errors = lintEndpointRules(postmanEndpoints, tsEndpoints);
+
+    // Assert that errors are present
+    expect(errors.length).toBeGreaterThan(0);
+    console.log('Linting errors found:' + errors);
+    // Optionally, you can check for specific error messages if you want to ensure specific types of mismatches are detected
+    // For example:
+    // expect(errors).toContain("Expected error message");
+  });
+});
+
